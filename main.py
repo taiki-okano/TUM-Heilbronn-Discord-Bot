034ab3e7417356dc8412ed6a540bf8dfd44a7a08
Taiki Okano <taiki_okano@me.com>
2021-09-07 01:38:17 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+
+
+TOKEN = "DISCORD_TOKEN"
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print("The bot has started successfully.")
+
+
+@client.event
+async def on_message(message):
+    """
+    This specifies how to react to the messages from users.
+    """
+
+    # Ignore messages from bots.
+    if message.author.bot:
+        return
+
+    if message.content == "/test":
+        await message.channel.send("Your test has succeeded")
+
+
+client.run(TOKEN)
